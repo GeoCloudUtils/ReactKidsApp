914052af454148a00060a366b8e3b0f279429b05
Gheorghe <gheorghe@edituraedu.ro>
2025-09-25 12:37:04 +0300

    Top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,3 +1,19 @@
 export function TopBar(): React.ReactElement {
-  return <div className="top-bar"></div>;
+  return (
+    <div className="top-bar">
+      //add number and email
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <span className="me-3">
+              <i className="fa fa-phone me-2"></i>+123 456 7890
+            </span>
+            <span>
+              <i className="fa fa-envelope me-2"></i>info@edukids.com
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
