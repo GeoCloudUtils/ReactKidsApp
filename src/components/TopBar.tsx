a91f40c4c723963754f12be2e2939b06b1a9b14d
Gheorghe <gheorghe@edituraedu.ro>
2025-09-25 12:49:26 +0300

    Top bar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,17 +1,14 @@
 export function TopBar(): React.ReactElement {
   return (
     <div className="top-bar bg-primary text-white">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6"></div> {/* Left empty for spacing */}
-          <div className="col-md-6 text-md-end text-center">
-            <span className="me-3">
-              <i className="fa fa-phone me-2"></i>+123 456 7890
-            </span>
-            <span>
-              <i className="fa fa-envelope me-2"></i>info@edukids.com
-            </span>
-          </div>
+      <div className="row">
+        <div className="col-md-12 text-md-end text-center">
+          <span className="me-3">
+            <i className="fa fa-phone me-2"></i>+123 456 7890
+          </span>
+          <span className="m-2">
+            <i className="fa fa-envelope me-2"></i>info@edukids.com
+          </span>
         </div>
       </div>
     </div>
